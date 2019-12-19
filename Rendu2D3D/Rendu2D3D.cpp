e5652d47b917dd099849029dac086d895a1b8436
Régis <regis.poizat@cegetel.net>
2019-12-19 23:30:39 +0100

    bruh

--- Rendu2D3D/Rendu2D3D.cpp
+++ Rendu2D3D/Rendu2D3D.cpp
@@ -10,6 +10,7 @@
 #include <cmath>
 #include "mat.h"
 #include <../../common/GLShader.h>
+#include "tiny_obj_loader.h"
 
 // les repertoires d'includes sont:
 // ../libs/glfw-3.3/include			fenetrage
@@ -35,16 +36,44 @@
 #endif
 
 #include <iostream>
+using namespace std;
 
 GLShader basic_shader;
 GLuint VBO;
 GLuint VAO;
 
-struct Vertex 
+GLuint IBOobj;
+GLuint VBOobj;
+GLuint VAOobj;
+
+struct Vertex
 {
 	float x, y, z;
 	float r, g, b;
 };
+
+std::string fileName = "./models/suzanne.obj";
+tinyobj::attrib_t attrib;
+std::vector<tinyobj::shape_t> shapes;
+std::vector<tinyobj::material_t> materials;
+
+std::string warn;
+std::string err;
+
+//chargement des vertices de l'objet dans un tableau
+std::vector<Vertex> triangleObj;
+std::vector<unsigned int> indicesObj;
+
+
+
+bool operator ==(const Vertex& v1, const Vertex& v2)
+{
+	if (v1.x == v2.x && v1.y == v2.y && v1.z == v2.z)
+	{
+		return true;
+	}
+	else return false;
+}
 
 void Initialize()
 {
@@ -62,28 +91,80 @@ void Initialize()
 	basic_shader.LoadFragmentShader("basic.fs");
 	basic_shader.Create();
 
+	//chargement de l'obj
+	bool ok = tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, fileName.c_str());
+
+	if (!warn.empty()) 
+	{
+		std::cout << warn << std::endl;
+	}
+
+	if (!err.empty()) 
+	{
+		std::cerr << err << std::endl;
+	}
+
+	if (!ok) exit(1);
+
+	//pour chaque vertice du fichier
+	for (int i = 0; i < attrib.vertices.size() - 2; i+=3)
+	{
+		//on crée un nouveau Vertex et on cherche dans la liste s'il n'y est pas déjà
+		Vertex v = {attrib.vertices[i], attrib.vertices[i + 1], attrib.vertices[i + 2], 0.3, 0.3, 0.3};
+
+		unsigned int indice = 0;
+		bool trouve = false;
+
+		for each (Vertex ver in triangleObj)
+		{
+			//si on trouve le vertex dans la liste, on ajoute son indice au tableau des indices
+			if (ver == v && !trouve)
+			{
+				indicesObj.push_back(indice);
+				trouve = true;
+			}
+			indice++;
+		}
+
+		//s'il on n'a pas trouvé le vertex dans la liste, on l'ajoute ainsi que son indice
+		if (!trouve)
+		{
+			triangleObj.push_back(v);
+			indicesObj.push_back(indice);
+		}
+	}
+
+	glGenBuffers(1, &VAOobj);
+	glBindBuffer(GL_ARRAY_BUFFER, VAOobj);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(triangleObj), &triangleObj, GL_STATIC_DRAW);
+
+	glGenBuffers(1, &IBOobj);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBOobj);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indicesObj.size(), &indicesObj, GL_STATIC_DRAW);
 	
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	// Idem mais en OpenGL moderne
 	// defini une liste de sommets, 2D, de type float
-	const Vertex triangles[] = {
-		{-0.5f, -0.5f, -1, 1.0, 0.0, 0.0},
-		{+0.5f, +0.5f, -1, 0.0, 0.0, 1.0},
-		{-0.5f, +0.5f, -1, 0.0, 1.0, 0.0},
-		
-		// second triangle
-		{-0.5f, -0.5f, -1, 1.0, 0.0, 0.0},
-		{+0.5f, -0.5f, -1, 1.0, 1.0, 1.0},
-		{+0.5f, +0.5f, -1, 0.0, 0.0, 1.0}
-	};
+	//const Vertex triangles[] = {
+	//	{-0.5f, -0.5f, -1, 1.0, 0.0, 0.0},
+	//	{+0.5f, +0.5f, -1, 0.0, 0.0, 1.0},
+	//	{-0.5f, +0.5f, -1, 0.0, 1.0, 0.0},
+	//	
+	//	// second triangle
+	//	{-0.5f, -0.5f, -1, 1.0, 0.0, 0.0},
+	//	{+0.5f, -0.5f, -1, 1.0, 1.0, 1.0},
+	//	{+0.5f, +0.5f, -1, 0.0, 0.0, 1.0}
+	//};
 
-	glGenBuffers(1, &VBO);
+	/*glGenBuffers(1, &VBO);
 
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(triangles), triangles, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(triangles), triangles, GL_STATIC_DRAW);*/
 
 	int basicProgram = basic_shader.GetProgram();
 	glUseProgram(basicProgram);
@@ -98,8 +179,7 @@ void Initialize()
 	glEnableVertexAttribArray(location);
 	glEnableVertexAttribArray(locationColor);
 
-	glBindVertexArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	
 }
 
 
@@ -108,6 +188,12 @@ void Shutdown()
 	basic_shader.Destroy();
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+
+	glDeleteVertexArrays(1, &VAOobj);
+	glDeleteBuffers(1, &VBOobj);
+
+	glDeleteVertexArrays(1, &IBOobj);
+	glDeleteBuffers(1, &IBOobj);
 }
 
 void Display(GLFWwindow* window)
@@ -129,10 +215,6 @@ void Display(GLFWwindow* window)
 	//glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	//glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
 	//glEnableVertexAttribArray(0);
-
-	
-	// dessine deux triangles
-	glBindVertexArray(VAO);
 
 	//matrix
 	mat4 mat = mat4();
@@ -166,7 +248,11 @@ void Display(GLFWwindow* window)
 	glUniformMatrix4fv(identifiant_matTrans, 1, GL_FALSE, matTranslate.data);
 	glUniformMatrix4fv(identifiant_matProjPersp, 1, GL_FALSE, matProjPersp.data);
 
-	glDrawArrays(GL_TRIANGLES, 0, 6);
+
+	// dessine deux triangles
+	glBindVertexArray(IBOobj);
+	glDrawElements(GL_TRIANGLES, indicesObj.size(), GL_UNSIGNED_INT, NULL);
+	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 }
 
 int main(void)
@@ -184,6 +270,8 @@ int main(void)
 		glfwTerminate();
 		return -1;
 	}
+
+	glEnable(GL_DEPTH_TEST);
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
